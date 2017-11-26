57ec1750b5ed0827de66059da0a40d5370745b81
Ghost Hackz <admin@ghosthackz.ml>
2017-11-26 10:26:53 +0545

    Commit

--- index.js
+++ index.js
@@ -10,48 +10,36 @@ service.set('port', (process.env.PORT || 3000));
 service.get('/sign', function(request, response) {
 	response.setHeader('Content-Type', 'text/plain');
 	
-	mobileconfig.getSignedConfig([
-		PayloadType: 'Configuration',
-		PayloadVersion: 1,
-		PayloadIdentifier: 'ml.ghosthackz.mail',
-		PayloadUUID: uuid.v4(),
-		PayloadDisplayName: 'GhostHackz',
-		PayloadDescription: 'Install GhostHackz e-mail account.',
-		PayloadOrganization: 'WebParadise Pvt. Ltd.',	
-		PayloadContent: {
-			PayloadType: 'com.apple.mail.managed',
-			PayloadVersion: 1,
-			PayloadIdentifier: 'ml.ghosthackz.mail',
-			PayloadUUID: uuid.v4(),
-			PayloadDisplayName: 'IMAP Config',
-			PayloadDescription: 'Configures email account',
-			PayloadOrganization: 'WebParadise Pvt. Ltd.',
-			EmailAccountDescription: 'GhostHackz email account',
-			EmailAccountName: 'GhostHackz',
-			EmailAccountType: 'EmailTypeIMAP',
-			IncomingMailServerAuthentication: 'EmailAuthPassword',
-			IncomingMailServerHostName: 'mail.ghosthackz.ml',
-			IncomingMailServerPortNumber: 993,
-			IncomingMailServerUseSSL: true,
-			IncomingMailServerUsername: 'admin@ghosthackz.ml',
-			IncomingPassword: 'Samridhi861',
-			OutgoingPasswordSameAsIncomingPassword: true,
-			OutgoingMailServerAuthentication: 'EmailAuthPassword',
-			OutgoingMailServerHostName: 'mail.ghosthackz.ml',
-			OutgoingMailServerPortNumber: 465,
-			OutgoingMailServerUseSSL: true,
-			OutgoingMailServerUsername: 'admin@ghosthackz.ml',
-			PreventMove: false,
-			PreventAppSheet: false,
-			SMIMEEnabled: false,
-			allowMailDrop: true
+	var options = {
+		emailAddress: 'admin@ghosthackz.ml',
+		organization: 'GhostHackz',
+		identifier: 'ml.ghosthackz.mail',
+		displayName: 'GhostHackz Email Configuration',
+		displayDescription: 'Adds GhostHackz Email',
+		accountName: 'GhostHackz',
+		imap: {
+			hostname: 'mail.ghosthackz.ml',
+			port: 993,
+			secure: true,
+			username: 'admin@ghosthackz.ml',
+			password: 'Samridhi861'
+		},
+		smtp: {
+			hostname: 'mail.ghosthackz.ml',
+			port: 465,
+			secure: true,
+			username: 'admin@ghosthackz.ml',
+			password: 'Samridhi861'
+		},
+		keys: {
+			key: fs.readFileSync('./key.pem'),
+			cert: fs.readFileSync('./cert.pem')
 		}
-], {
-	key: fs.readFileSync('./key.pem'),
-	cert: fs.readFileSync('./cert.pem'),
-	ca: []
-}, function(error, data) {
-	response.send(data);
+	};
+	
+	mobileconfig.getSignedEmailConfig(options, function(error, data) {
+		response.send(data);
+	});
 });
 
 service.listen(service.get('port'), function() {
