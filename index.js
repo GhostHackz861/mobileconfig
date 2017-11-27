03e527d6859463aed20c6fce9446b953af1d122f
Ghost Hackz <admin@ghosthackz.ml>
2017-11-27 20:01:40 +0545

    Commit

--- index.js
+++ index.js
@@ -1,53 +1,89 @@
 'use strict';
 
-const express = require('express');
-const mobileconfig = require('mobileconfig');
+const handlebars = require('handlebars');
+const jsrsasign = require('jsrsasign');
+const uuid = require('uuid');
 const fs = require('fs');
-const service = express();
 
-service.set('port', (process.env.PORT || 3000));
+const templates = {
+		vpn: handlebars.compile(fs.readFileSync('./templates/vpn.plist', 'utf-8'))
+};
 
-service.get('/sign', function(request, response) {
-	response.setHeader('Content-Type', 'application/x-apple-aspen-config');
-	
-	var options = {
-		emailAddress: 'admin@ghosthackz.ml',
-		organization: 'GhostHackz',
-		identifier: 'ml.ghosthackz.mail',
-		displayName: 'GhostHackz Email Configuration',
-		displayDescription: 'Adds GhostHackz Email',
-		accountName: 'GhostHackz',
-		imap: {
-			hostname: 'mail.ghosthackz.ml',
-			port: 993,
-			secure: true,
-			username: 'admin@ghosthackz.ml',
-			password: 'Samridhi861'
-		},
-		smtp: {
-			hostname: 'mail.ghosthackz.ml',
-			port: 465,
-			secure: true,
-			username: 'admin@ghosthackz.ml',
-			password: 'Samridhi861'
-		},
-		keys: {
-			key: fs.readFileSync('./key.pem'),
-			cert: fs.readFileSync('./cert.pem'),
-			ca: [
-				fs.readFileSync('./c1.pem'),
-				fs.readFileSync('./c2.pem'),
-				fs.readFileSync('./c3.pem'),
-				fs.readFileSync('./c4.pem')
+module.exports = {
+	sign(value, options, callback) {
+		options = options || {};
+		
+		let cert = [];
+		[].concat(options.cert || []).concat(options.ca || []).map(ca => {
+			ca = (ca || '').toString().trim().split('END CERTIFICATE-----');
+			ca.pop();
+			ca.forEach(ca => {
+				ca += 'END CERTIFICATE-----';
+				certs.push(ca.trim());
+			});
+			return ca;
+		});
+		
+		certs = certs.reverse();
+		
+		let der;
+		let params = {
+			content: {
+				str: (value || '').toString('utf-8')
+			},
+			certs,
+			signerInfos: [
+				{
+					hashAlg: options.hashAlg || 'sha256',
+					sAttr: options.signingTime ? {
+						SigningTime: {}
+					} : {},
+					signerCert: certs[certs.length - 1],
+					signerPrvKey: (options.key || '').toString(),
+					sigAlg: options.sigAlg || 'SHA256withRSA'
+				}
 			]
+		};
+		
+		try {
+			der = Buffer.from(jsrsasign.asn1.cms.CMSUtil.newSignedData(params).getContentInfoEncodedHex(), 'hex');
+		} catch(E) {
+			return setImmediate(() => {
+				callback(E);
+			});
 		}
-	};
+		
+		return setImmediate(() => {
+			callback(null, der);
+		});
+	},
 	
-	mobileconfig.getSignedEmailConfig(options, function(error, data) {
-		response.send(data);
-	});
-});
-
-service.listen(service.get('port'), function() {
-	console.log('Connected to the server!');
-});
+	getVPNConfig(options, callback) {
+		let data = {
+			displayName: options.displayName || 'VPN',
+			contentUUID: uuid.v4(),
+			plistUUID: uuid.v4()
+		};
+		
+		if (callback) {
+			callback(null, templates.vpn(data));
+			return;
+		}
+		
+		return templates.vpn(data);
+	},
+	
+	getSignedConfig(options, callback) {
+		options = options || {};
+		
+		let plistFile;
+		
+		try {
+			plistFile = module.exports.getVPNConfig(options);
+		} catch(E) {
+			return callback(E);
+		}
+		
+		return module.exports.sign(plistFile, options.keys, callback);
+	}
+};
